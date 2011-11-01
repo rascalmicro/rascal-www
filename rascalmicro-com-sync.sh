9bdf446b3ff0386ffae88e5a912b176add75a711
Brandon Stafford <brandon@pingswept.org>
2011-11-01 15:42:03 -0400

    Make rsync avoid huge directories of files that should never change

--- rascalmicro-com-sync.sh
+++ rascalmicro-com-sync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync -av --delete --exclude '*cache/*' ./_site/ root@rascalmicro.com:/var/www/rascalmicro.com/
+rsync -av --delete --exclude-from './exclude.txt' ./_site/ root@rascalmicro.com:/var/www/rascalmicro.com/
 
 ssh root@rascalmicro.com "chown -R www-data:www-data /var/www/*"
